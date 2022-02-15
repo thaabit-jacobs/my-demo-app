8c40bd0b2e83401620b73f74e9ce1c4580bbd454
thaabit <mohammedthaabitjacobs@gmail.com>
2022-02-15 20:22:18 +0200

    jsx

--- src/index.js
+++ src/index.js
@@ -5,24 +5,37 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 let user = {
-  fName:"Thaabit",
-  lName:"Jacobs",
+  firstName:"Thaabit",
+  lastName:"Jacobs",
 }
 
 function formatter(user){
-  return user.fName + " " + user.lName;
+  return user.firstName + " " + user.lastName;
 }
 
-const  nameElement  = (
-  <h1>
-    Hello there, {formatter(user)} welcome to React
-  </h1>
+let counter = 0;
+
+const element = (
+  <div>
+    <h1>Hello {formatter(user)}</h1>
+    <h2>Good to see you here.</h2>
+  </div>
 );
 
-ReactDOM.render(
-  nameElement,
-  document.getElementById("root")
-);
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  const counterElment = (
+    <div>
+      <h1>Current Count: {++counter}</h1>
+    </div>
+  )
+  
+  ReactDOM.render(
+    counterElment,
+    document.getElementById("root")
+  );
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
