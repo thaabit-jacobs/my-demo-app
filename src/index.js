fa4a7d57b583dcb1eec90d7a94cca6207f27b1d3
thaabit <mohammedthaabitjacobs@gmail.com>
2022-02-20 14:20:23 +0200

    list and keyys

--- src/index.js
+++ src/index.js
@@ -121,9 +121,29 @@ class Toggle extends React.Component {
   }
 }
 
+/*
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
+*/
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>{number}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
 ReactDOM.render(
-  <Toggle />,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
+
 
 reportWebVitals();
